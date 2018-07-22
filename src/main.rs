a75fefdd84b537319964a2cbaf3540b85488be78
Peter Foley <pefoley2@pefoley.com>
2018-07-22 00:09:06 -0400

    update to irc v0.13

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate irc;
 use irc::client::prelude::*;
 
 fn main() {
-    let server = IrcServer::new("config.json").unwrap();
-    server.identify().unwrap();
+    let mut reactor = IrcReactor::new().unwrap();
+    let config = Config::load("config.toml").unwrap();
+    let client = reactor.prepare_client_and_connect(&config).unwrap();
+    client.identify().unwrap();
 }
